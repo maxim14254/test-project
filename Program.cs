30e8a61afddfa896f087d75bb195086986c1fcc4
maxim14254 <69618032+maxim14254@users.noreply.github.com>
2020-09-02 09:38:59 +0300

    был исправлен баг с повторным добавлением в базу данных

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 
@@ -15,6 +14,7 @@ namespace ConsoleApp1
         private static List<string> albums = new List<string>() { };
         private static string sql;
         private static SqlCommand command;
+        private static bool block = true;//переменная открывающая проход для добавление артиста и его альбомов
         public static void Main(string[] args)
         {
             SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True;Connect Timeout=30");
@@ -34,9 +34,9 @@ namespace ConsoleApp1
                 Console.WriteLine("Нет сети! Проверте подключение\n");//если нет подключения, осуществляем загрузку данных и нашей БД
                 sql = String.Format("SELECT albums.name FROM albums INNER JOIN groups ON albums.group_id = Groups.Id WHERE Groups.name = N'{0}'", group);
                 command = new SqlCommand(sql, sqlConnection);
-               
+
                 SqlDataReader reader = command.ExecuteReader(); // сохраняем результаты поиска в объект reader
-               
+
                 while (reader.Read()) // перебираем
                 {
                     Console.WriteLine(reader.GetValue(0).ToString());// выводим на экран
@@ -47,18 +47,18 @@ namespace ConsoleApp1
             }
 
             string directory = System.IO.Path.GetDirectoryName(path) + "\\artistId.txt";// сохраняем путь загруженного файла
-            
+
             StreamReader streamReader = new StreamReader(directory.ToString());// объект класса для работы с загруженным файлом
-            string str="";
-           
+            string str = "";
+
             while (!streamReader.EndOfStream)//перебираем данные у загруженного файла (artistId.txt)
             {
                 str += streamReader.ReadLine();
             }
 
             Match regex = Regex.Match(str, @".artistId.:(.*?),");// шаблон для поиска ID артиста в Itunes
-           
-            if(regex.Groups[1].Value=="")// если ID не обнаруживатся, то артиста нет а БД Itunes
+
+            if (regex.Groups[1].Value == "")// если ID не обнаруживатся, то артиста нет а БД Itunes
             {
                 Console.WriteLine("Такой группы (исполнителя) нет");
                 Console.ReadKey();
@@ -109,40 +109,44 @@ namespace ConsoleApp1
             Process.GetCurrentProcess().Kill();
         }
         private static void SetData(int i, SqlConnection sqlConnection, string group)
-        { 
-            if (i == 0)//выполняется 1 раз 
+        {
+            if (i == 0 && block) //выполняется 1 раз 
             {
-                bool b = true;//переменная открывающая проход для добавление артиста 
-                sql= String.Format("SELECT name FROM groups");//запрос на вывод всех артистов в БД
+                sql = String.Format("SELECT name FROM groups");//запрос на вывод всех артистов в БД
                 command = new SqlCommand(sql, sqlConnection);
-                
+
                 SqlDataReader reader = command.ExecuteReader();//сохраняем всех артистов в объекте reader
-                
+                int a;
                 while (reader.Read())
                 {
-                    int а = String.Compare(group, reader.GetValue(0).ToString());// сравниваем артиста которого ввели и который находится в БД
-                    if (а==0)//если они совпали, то закрываем проход на добавление нового артиста в БД
+                    a = String.Compare(group, reader.GetValue(0).ToString());// сравниваем артиста которого ввели и который находится в БД
+                    if (a == 0)//если они совпали, то закрываем проход на добавление нового артиста в БД
                     {
-                        b = false;
+                        block = false;
                         break;
                     }
                 }
+
                 reader.Close();
-                
-                if(b)// проход на добавление артиста в БД
+
+                if (block)// проход на добавление артиста в БД
                 {
                     sql = String.Format("INSERT groups (name) VALUES (N'{0}')", group.Replace('\'', '`'));
                     command = new SqlCommand(sql, sqlConnection);
                     command.ExecuteNonQuery();
                 }
             }
-            sql= String.Format("SELECT id FROM groups WHERE name = N'{0}'", group);//запрос на вывод ID артиста
-            command = new SqlCommand(sql, sqlConnection);
-            string id = command.ExecuteScalar().ToString();// сохраняем ID
 
-            sql = String.Format("INSERT albums (name,group_id) VALUES (N'{0}','{1}')", albums[i].Replace('\'', '`'), id);//запрос на запись альбома[i] в БД
-            command = new SqlCommand(sql, sqlConnection);
-            command.ExecuteNonQuery();
+            if (block)// проход на добавление альбомов в БД
+            {
+                sql = String.Format("SELECT id FROM groups WHERE name = N'{0}'", group);//запрос на вывод ID артиста
+                command = new SqlCommand(sql, sqlConnection);
+                string id = command.ExecuteScalar().ToString();// сохраняем ID
+
+                sql = String.Format("INSERT albums (name,group_id) VALUES (N'{0}','{1}')", albums[i].Replace('\'', '`'), id);//запрос на запись альбома[i] в БД
+                command = new SqlCommand(sql, sqlConnection);
+                command.ExecuteNonQuery();
+            }
         }
     }
 
